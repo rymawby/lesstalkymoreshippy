5beb570588609637946fe37e94eadb116ac476c0
Ryan Mawby <ryan@wegoodhq.com>
2014-01-06 22:43:39 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lesstalkymoreshippy::Application.config.secret_key_base = 'd76510c5571970bb527920ed7f3ef0505a797a871bc68178790fd3b640f174ce44ee554facffad362a1994f391e664fb9693e34ebf8920409925d42ed85692e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lesstalkymoreshippy::Application.config.session_store :cookie_store, key: '_lesstalkymoreshippy_session'
