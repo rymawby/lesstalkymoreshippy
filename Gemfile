ceef1af57dab9cf5eea794b0f368278a6163abc6
Ryan Mawby <ryan@wegoodhq.com>
2014-01-07 15:41:06 +0000

    Added factory girl

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ group :development do
   gem 'rb-fchange', :require=>false
   gem 'rb-fsevent', :require=>false
   gem 'rb-inotify', :require=>false
+  gem 'factory_girl'
 end
 group :development, :test do
   gem 'rspec-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140106225609) do
+ActiveRecord::Schema.define(version: 20140107144416) do
 
   create_table "roles", force: true do |t|
     t.string   "name"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20140106225609) do
 
   add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
   add_index "roles", ["name"], name: "index_roles_on_name"
+
+  create_table "roles_users", id: false, force: true do |t|
+    t.integer "role_id"
+    t.integer "user_id"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
