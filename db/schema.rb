5576f2d0cb440c46b737b53f66f60a9dbc2e6df6
Ryan Mawby <ryan@wegoodhq.com>
2014-01-07 18:12:04 +0000

    Updated to rails 4 syntax

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140107144416) do
+ActiveRecord::Schema.define(version: 20140107171839) do
 
   create_table "roles", force: true do |t|
     t.string   "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20140107144416) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
