706003f3e832c9c082c95c3bc47b399d5f89a09b
Ryan Mawby <ryan@wegoodhq.com>
2014-01-13 15:08:41 +0000

    Completed tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   
-  has_many :projects
+  has_many :projects, foreign_key: 'creator_id'
+  has_many :projects, foreign_key: 'validator_id'
   has_many :targets, through: :projects
 
   rolify
