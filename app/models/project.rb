79e339ba2622d29b40b5153013a1a5ef39b4b0ee
Ryan Mawby <ryan@wegoodhq.com>
2014-01-13 14:38:26 +0000

    has many targets test

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,6 @@ class Project < ActiveRecord::Base
 
 	belongs_to :validator, :class_name => 'User', :foreign_key => 'validator_id'
 	belongs_to :creator, :class_name => 'User', :foreign_key => 'creator_id'
+	has_many :targets
 
 end
